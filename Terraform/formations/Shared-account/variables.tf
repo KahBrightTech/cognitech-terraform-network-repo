36f9af6d899f00b762a2478480f6b246e2f5b765
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-17 20:42:06 -0400

    update on variables

--- Terraform/formations/Shared-account/variables.tf
+++ Terraform/formations/Shared-account/variables.tf
@@ -73,7 +73,12 @@ variable "vpcs" {
       has_quaternary_subnet  = optional(bool, false)
     })
     transit_gateway = optional(object({
-      name = string
+      name                            = string
+      default_route_table_association = enable
+      default_route_table_propagation = enable
+      auto_accept_shared_attachments  = disable
+      dns_support                     = enable
+      amazon_side_asn                 = 64512
     }))
   }))
   default = null
