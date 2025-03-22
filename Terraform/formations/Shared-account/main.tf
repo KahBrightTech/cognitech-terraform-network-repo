97ea9c40f3f8ceaa2e615f882db3aeaed2bbfa01
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-21 23:22:17 -0400

    tgw update

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -1,7 +1,15 @@
 module "public_route" {
   source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Routes/public_routes?ref=v1.35"
+  vpc_id = module.vpc.vpc_id
+  public_routes = {
+    public_gateway_id      = module.vpc.igw_id
+    destination_cidr_block = var.vpcs.public_routes.destination_cidr_block
+    primary_subnet_id      = module.public_subnets.primary_subnet_id
+    secondary_subnet_id    = module.public_subnets.secondary_subnet_id
+    tertiary_subnet_id     = module.public_subnets.tertiary_subnet_id
+  }
+  common = var.common
 }
-
 module "shared_vpc" {
   source   = "../Create-Network"
   for_each = { for vpc in var.vpcs : vpc.name => vpc }
