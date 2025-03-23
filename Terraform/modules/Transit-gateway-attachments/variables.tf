8e236704b72547d2087c798b6c17f49d3b8c1e73
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-22 21:20:44 -0400

    Another update

--- Terraform/modules/Transit-gateway-attachments/variables.tf
+++ Terraform/modules/Transit-gateway-attachments/variables.tf
@@ -13,8 +13,8 @@ variable "tgw_attachments" {
   description = "The transit gateway attachment variables"
   type = object({
     transit_gateway_id   = string
-    primary_subnet_id    = optional(string)
-    secondary_subnet_id  = optional(string)
+    primary_subnet_id    = string
+    secondary_subnet_id  = string
     transit_gateway_name = optional(string)
     shared_vpc_name      = optional(string)
     vpc_id               = string
