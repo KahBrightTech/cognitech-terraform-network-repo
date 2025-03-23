638cbcf337e2620ebb02cd1578de7a10cb0f15db
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-23 12:24:56 -0400

    Testing

--- Terraform/modules/Transit-gateway-attachments/main.tf
+++ Terraform/modules/Transit-gateway-attachments/main.tf
@@ -2,7 +2,7 @@
 # Transit Gateway - Creates Transit Gateway
 #--------------------------------------------------------------------
 module "transit_gateway_attachments" {
-  source          = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Transit-gateway-attachments?ref=v1.54"
+  source          = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Transit-gateway-attachments?ref=v1.56"
   common          = var.common
   tgw_attachments = var.tgw_attachments
   vpc_id          = var.vpc_id
