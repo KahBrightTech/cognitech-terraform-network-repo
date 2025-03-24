11c116e137018632fb0882490bab0512a98f97f0
Brigthain Kargong <kbrigthain@gmail.com>
2025-03-23 22:12:38 -0400

    Testing

--- Terraform/formations/Tenant-account/main.tf
+++ Terraform/formations/Tenant-account/main.tf
@@ -11,7 +11,7 @@ data "aws_ec2_transit_gateway" "tgw" {
 data "aws_vpc" "shared_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["${var.common.account_name}-${var.common.region_prefix}-${var.tgw_attachments.shared_vpc_name}"]
+    values = ["${var.common.account_name}-${var.common.region_prefix}-${var.tgw_attachments.shared_vpc_name}-vpc"]
   }
 }
 
