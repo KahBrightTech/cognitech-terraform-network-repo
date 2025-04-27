90f3d9bac211d1c835b47428062f92288175bd6c
Brigthain Kargong <kbrigthain@gmail.com>
2025-04-27 18:15:00 -0400

    creating resources

--- Terraform/formations/Create-Lambdas/main.tf
+++ Terraform/formations/Create-Lambdas/main.tf
@@ -9,7 +9,7 @@ data "aws_caller_identity" "current" {}
 # Creates lambda function to be used in the service catalog to start instances
 #--------------------------------------------------------------------
 module "Start_Instance" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Lambdas/Template?ref=v1.100"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Lambdas/Template?ref=v1.101"
   for_each = (var.Lambdas != null) ? { for item in var.Lambdas : item.function_name => item } : {}
   common   = var.common
   Lambda   = each.value
