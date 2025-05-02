cbd91c0f553108397c8f9557c41a793920f1877c
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-02 15:57:09 -0400

    testing

--- Terraform/modules/Create-secrets/main.tf
+++ Terraform/modules/Create-secrets/main.tf
@@ -3,7 +3,7 @@
 # Creates secrets
 #--------------------------------------------------------------------
 module "secrets" {
-  source          = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Secrets-manager?ref=v1.117"
+  source          = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Secrets-manager?ref=v1.118"
   for_each        = (var.secrets != null) ? { for item in var.secrets : item.name => item } : {}
   common          = var.common
   secrets_manager = each.value
