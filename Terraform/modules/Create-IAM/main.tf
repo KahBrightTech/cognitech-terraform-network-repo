553068b845b7347bb58d560e003da030654e6cad
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-02 12:22:52 -0400

    Testinf123

--- Terraform/modules/Create-IAM/main.tf
+++ Terraform/modules/Create-IAM/main.tf
@@ -9,7 +9,7 @@ data "aws_caller_identity" "current" {}
 # Creates lambda function to be used in the service catalog to start instances
 #--------------------------------------------------------------------
 module "iam_user" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/IAM-User?ref=v1.111"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/IAM-User?ref=v1.112"
   for_each = (var.iam_users != null) ? { for item in var.iam_users : item.name => item } : {}
   common   = var.common
   iam_user = each.value
