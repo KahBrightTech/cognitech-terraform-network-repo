971d86a121785fd1d91cf87d05ed6de4264ed485
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-08 21:37:06 -0400

    Testing

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -77,14 +77,14 @@ module "transit_gateway_route" {
 #--------------------------------------------------------------------
 
 module "s3_app_bucket" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/S3-Private-bucket?ref=v1.94"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/S3-Private-bucket?ref=v1.22"
   for_each = (var.s3_private_buckets != null) ? { for item in var.s3_private_buckets : item.name => item } : {}
   common   = var.common
   s3 = {
     name              = each.value.name
     description       = each.value.description
     enable_versioning = each.value.enable_versioning
-    policy            = var.s3_private_buckets.policy
+    policy            = each.value.policy
     iam_role_arn_pattern = {
       "[[sso_network_role_arn]]" = tolist(data.aws_iam_roles.network_role.arns)[0]
       # "[[sso_admin_role_arn]]"   = tolist(data.aws_iam_roles.admin_role.arns)[0]
