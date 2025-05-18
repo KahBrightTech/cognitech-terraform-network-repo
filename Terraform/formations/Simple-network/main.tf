71454c4062cd741ea48f15c38610107b6d3bddf4
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-18 19:41:10 -0400

    Test

--- Terraform/formations/Simple-network/main.tf
+++ Terraform/formations/Simple-network/main.tf
@@ -22,13 +22,15 @@ module "public_subnets" {
 
 
 module "public_route" {
-  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Routes/public_routes?ref=v1.1.5"
+  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Routes/public_routes?ref=v1.1.7"
   vpc_id = module.vpc.vpc_id
   common = var.common
   public_routes = {
     public_gateway_id      = module.vpc.igw_id
     destination_cidr_block = var.vpc.public_routes.destination_cidr_block
-    subnet_ids             = module.public_subnets[each.value.name].subnet_ids
+    primary_subnet_id      = module.public_subnets[each.value.name].primary_subnet_id
+    secondary_subnet_id    = module.public_subnets[each.value.name].secondary_subnet_id
+    tertiary_subnet_id     = module.public_subnets[each.value.name].tertiary_subnet_id
   }
 }
 
