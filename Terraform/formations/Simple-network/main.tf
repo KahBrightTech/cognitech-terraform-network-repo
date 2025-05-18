20e06c55152c9e1b2c127d9729c8c9473ce4b237
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-18 19:09:14 -0400

    Testing

--- Terraform/formations/Simple-network/main.tf
+++ Terraform/formations/Simple-network/main.tf
@@ -12,11 +12,11 @@ module "vpc" {
 #--------------------------------------------------------------------
 
 module "public_subnets" {
-  source         = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnet/public_subnets?ref=v1.1.4"
+  source         = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets/public_subnets?ref=v1.1.3"
   for_each       = var.vpc != null && var.vpc.public_subnets != null ? { for public_subnet in var.vpc.public_subnets : public_subnet.name => public_subnet } : {}
   vpc_id         = module.vpc.vpc_id
   public_subnets = each.value
-  common         = var.common
+  common         = each.value
 }
 
 
