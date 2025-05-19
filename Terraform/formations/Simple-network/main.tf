daa0649413e8daa73d96508b10b45114b4768d67
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-18 20:17:57 -0400

    Test

--- Terraform/formations/Simple-network/main.tf
+++ Terraform/formations/Simple-network/main.tf
@@ -36,11 +36,11 @@ module "public_route" {
 }
 
 module "private_subnets" {
-  source         = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets/private_subnets?ref=v1.1.3"
-  for_each       = var.vpc != null && var.vpc.public_subnets != null ? { for public_subnet in var.vpc.public_subnets : public_subnet.name => public_subnet } : {}
-  vpc_id         = module.vpc.vpc_id
-  public_subnets = each.value
-  common         = var.common
+  source          = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets/private_subnets?ref=v1.1.3"
+  for_each        = var.vpc != null && var.vpc.private_subnets != null ? { for private_subnet in var.vpc.private_subnets : private_subnet.name => private_subnet } : {}
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = each.value
+  common          = var.common
 }
 
 
