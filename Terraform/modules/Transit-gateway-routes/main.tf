7e8728779178f4f4b54f0c5f5843dbdf90e4916c
Brigthain Kargong <kbrigthain@gmail.com>
2025-05-27 21:13:25 -0400

    Updating tgw routes

--- Terraform/modules/Transit-gateway-routes/main.tf
+++ Terraform/modules/Transit-gateway-routes/main.tf
@@ -2,7 +2,7 @@
 # Transit Gateway - Creates Transit Gateway
 #--------------------------------------------------------------------
 module "transit_gateway_routes" {
-  source     = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Transit-gateway-routes?ref=v1.1.1"
+  source     = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Transit-gateway-routes?ref=v1.1.15"
   common     = var.common
   tgw_routes = var.tgw_routes
 }
