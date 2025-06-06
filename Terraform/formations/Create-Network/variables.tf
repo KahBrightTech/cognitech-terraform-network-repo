93885ddf06862877169fe90bf6cbedb558576bf6
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-05 21:35:15 -0400

    Testing

--- Terraform/formations/Create-Network/variables.tf
+++ Terraform/formations/Create-Network/variables.tf
@@ -28,7 +28,6 @@ variable "vpc" {
       tertiary_cidr_block            = optional(string)
       subnet_ids                     = optional(list(string))
       subnet_type                    = optional(string)
-      vpc_name                       = string
     }))
     private_subnets = list(object({
       name                            = string
@@ -45,13 +44,11 @@ variable "vpc" {
       quaternary_availability_zone_id = optional(string)
       quaternary_cidr_block           = optional(string)
       subnet_type                     = optional(string)
-      vpc_name                        = string
     }))
     public_routes = optional(object({
       public_gateway_id      = optional(string)
       destination_cidr_block = optional(string)
       subnet_ids             = optional(list(string))
-      vpc_name               = string
     }))
     nat_gateway = optional(object({
       name              = string
@@ -60,7 +57,6 @@ variable "vpc" {
       secondary_subnet  = optional(string)
       tertiary_subnet   = optional(string)
       quaternary_subnet = optional(string)
-      vpc_name          = string
     }))
     private_routes = object({
       nat_gateway_id         = optional(string)
@@ -71,14 +67,12 @@ variable "vpc" {
       quaternary_subnet_id   = optional(string)
       has_tertiary_subnet    = optional(bool, false)
       has_quaternary_subnet  = optional(bool, false)
-      vpc_name               = string
     })
     security_groups = optional(list(object({
       key         = string
       description = string
       name        = string
       name_prefix = string
-      vpc_name    = string
       egress = optional(list(object({
         name            = string
         description     = string
