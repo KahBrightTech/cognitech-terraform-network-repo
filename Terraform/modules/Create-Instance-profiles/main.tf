f41915d668c097afb1da38a0060f4b0c73f2d3bd
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-10 00:34:06 -0400

    testing

--- Terraform/modules/Create-Instance-profiles/main.tf
+++ Terraform/modules/Create-Instance-profiles/main.tf
@@ -2,7 +2,7 @@
 # Module to create EC2 instance profiles
 #--------------------------------------------------------------------
 module "ec2_profiles" {
-  source       = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/EC2-profiles?ref=v1.1.41"
+  source       = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/EC2-profiles?ref=v1.1.42"
   for_each     = (var.ec2_profiles != null) ? { for item in var.ec2_profiles : item.name => item } : {}
   common       = var.common
   ec2_profiles = each.value
