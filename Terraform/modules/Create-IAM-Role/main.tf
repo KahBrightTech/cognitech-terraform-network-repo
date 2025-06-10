29aa4915ab3ad726e2e5b034cdc5821ff5c3af3c
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-09 23:24:19 -0400

    Test

--- Terraform/modules/Create-IAM-Role/main.tf
+++ Terraform/modules/Create-IAM-Role/main.tf
@@ -2,7 +2,7 @@
 # IAM Roles and Policies
 #--------------------------------------------------------------------
 module "IAM_roles" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/IAM-Roles?ref=v1.1.33"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/IAM-Roles?ref=v1.1.35"
   for_each = (var.iam_roles != null) ? { for item in var.iam_roles : item.name => item } : {}
   common   = var.common
   iam_role = each.value
