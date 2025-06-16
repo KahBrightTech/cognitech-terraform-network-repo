69e2e3f8cf35815a59c32d8c5b0af3969cfbbccc
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-15 20:25:00 -0400

    simple networ creation

--- Terraform/formations/Daily-modules/Simple-network/main.tf
+++ Terraform/formations/Daily-modules/Simple-network/main.tf
@@ -107,7 +107,7 @@ module "security_group_rules" {
 }
 
 module "s3_data_bucket" {
-  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/S3-Private-bucket?ref=v1.1.1"
+  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/S3-Private-bucket?ref=v1.1.47"
   common = var.common
   s3 = {
     name        = "${var.vpc.s3.name}"
