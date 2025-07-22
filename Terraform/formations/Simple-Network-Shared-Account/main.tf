8df0fd23c076d30e724336076a27432f9bb07563
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-21 23:30:11 -0400

    testing

--- Terraform/formations/Simple-Network-Shared-Account/main.tf
+++ Terraform/formations/Simple-Network-Shared-Account/main.tf
@@ -194,7 +194,7 @@ module "target_groups" {
 #--------------------------------------------------------------------
 module "alb_listeners" {
   source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/alb-listeners?ref=v1.2.69"
-  for_each = (var.alb_listeners != null) ? { for item in var.alb_listeners : item.name => item } : {}
+  for_each = (var.alb_listeners != null) ? { for item in var.alb_listeners : item.key => item } : {}
   common   = var.common
   alb_listener = merge(
     each.value,
@@ -218,7 +218,7 @@ module "alb_listeners" {
 #--------------------------------------------------------------------
 module "nlb_listeners" {
   source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/nlb-listener?ref=v1.2.69"
-  for_each = (var.nlb_listeners != null) ? { for item in var.nlb_listeners : item.name => item } : {}
+  for_each = (var.nlb_listeners != null) ? { for item in var.nlb_listeners : item.key => item } : {}
   common   = var.common
   nlb_listener = merge(
     each.value,
