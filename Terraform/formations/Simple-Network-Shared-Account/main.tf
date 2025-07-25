089a468c490bdf6668e6968988d4907e571fd401
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-24 23:52:43 -0400

    lb update

--- Terraform/formations/Simple-Network-Shared-Account/main.tf
+++ Terraform/formations/Simple-Network-Shared-Account/main.tf
@@ -109,7 +109,7 @@ module "backups" {
 # Createss load balancers
 #--------------------------------------------------------------------
 module "load_balancers" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Load-Balancers?ref=v1.2.89"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Load-Balancers?ref=v1.2.91"
   for_each = (var.load_balancers != null) ? { for item in var.load_balancers : item.key => item } : {}
   common   = var.common
   load_balancer = merge(
