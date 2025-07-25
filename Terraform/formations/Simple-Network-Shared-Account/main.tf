08ea986586bedb6ea0b4248726ea63c6da320834
Brigthain Kargong <kbrigthain@gmail.com>
2025-07-25 13:46:42 -0400

    testing again

--- Terraform/formations/Simple-Network-Shared-Account/main.tf
+++ Terraform/formations/Simple-Network-Shared-Account/main.tf
@@ -237,7 +237,7 @@ module "alb_listener_rules" {
       {
         listener_arn = each.value.listener_key != null ? module.alb_listeners[each.value.listener_key].alb_listener_arn : each.value.listener_arn
         target_groups = [
-          for tg in each.value.target_groups :
+          for tg in item.target_groups :
           {
             arn    = tg.tg_name != null ? module.target_groups[tg.tg_name].target_group_arn : tg.arn
             weight = tg.weight
