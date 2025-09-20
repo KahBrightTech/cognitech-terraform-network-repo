1ed1610ea01fed596a11d1278d5096076b31e99e
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-09-20 00:00:58 -0400

    creating datasync tasks

--- Terraform/formations/Simple-Network-Shared-Account/outputs.tf
+++ Terraform/formations/Simple-Network-Shared-Account/outputs.tf
@@ -125,3 +125,20 @@ output "iam_users" {
   description = "Output for IAM Users"
   value       = module.iam_users
 }
+
+#-------------------------------------------------------
+# DataSync Locations outputs
+#-------------------------------------------------------
+output "datasync_locations" {
+  description = "Output for DataSync Locations"
+  value       = module.datasync_locations
+}
+
+
+#-------------------------------------------------------
+# DataSync Tasks outputs
+#-------------------------------------------------------
+output "datasync_tasks" {
+  description = "Output for DataSync Tasks"
+  value       = module.datasync_tasks
+}
