f2bf5c8f76810c9b41d60cb4be0eba1cdc1fcda0
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-10-14 21:54:02 -0400

    test

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -131,7 +131,7 @@ module "transit_gateway_subnet_route" {
 #--------------------------------------------------------------------
 module "ram" {
   source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/RAM?ref=v1.3.69"
-  count  = var.transit_gateway.ram != null && var.transit_gateway.ram.enabled == true ? 1 : 0
+  count  = var.transit_gateway != null && var.transit_gateway.ram != null && var.transit_gateway.ram.enabled == true && length(module.transit_gateway) > 0 ? 1 : 0
   common = var.common
   depends_on = [
     module.transit_gateway
