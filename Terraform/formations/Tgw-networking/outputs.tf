600301e95430497e3c4959491c7ed214856f492a
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-10-19 15:40:04 -0400

    tgw-routes

--- Terraform/formations/Tgw-networking/outputs.tf
+++ Terraform/formations/Tgw-networking/outputs.tf
@@ -1,13 +1,3 @@
-
-output "transit_gateway" {
-  description = "Transit Gateway details"
-  value       = length(module.transit_gateway) > 0 ? module.transit_gateway[0] : null
-}
-output "transit_gateway_attachment" {
-  description = "Transit Gateway attachment details"
-  value       = length(module.transit_gateway_attachment) > 0 ? module.transit_gateway_attachment[0] : null
-}
-
 output "transit_gateway_route_table" {
   description = "Transit Gateway route table details"
   value       = length(module.transit_gateway_route_table) > 0 ? module.transit_gateway_route_table : null
