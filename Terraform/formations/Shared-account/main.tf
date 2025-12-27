85b589bc083388621a0a7a34ac874c9c35a28a19
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-26 19:37:03 -0500

    addons

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -767,7 +767,7 @@ module "eks_worker_nodes" {
 # EKS Addons
 #--------------------------------------------------------------------
 module "eks_cluster_addons" {
-  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/EKS-Cluster-Addons?ref=v1.4.96"
+  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/EKS-Cluster-Addons?ref=v1.4.97"
   for_each = merge([
     for cluster in var.eks_clusters :
     (cluster.eks_addons != null && lookup(cluster, "create_node_group", true)) ?
