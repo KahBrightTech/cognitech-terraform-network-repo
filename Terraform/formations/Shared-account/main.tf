ff7dae3d497e39d1e29f9777b552ad5313213393
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2025-12-27 20:01:37 -0500

    addons

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -571,7 +571,7 @@ module "waf" {
 # Creates EKS and supporting resources
 #--------------------------------------------------------------------
 module "eks" {
-  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Deploy-eks?ref=v1.5.16"
+  source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Deploy-eks?ref=v1.5.17"
   for_each = (var.var.eks != null) ? { for item in var.var.eks : item.create_eks_cluster ? item.key : null => item if item.create_eks_cluster } : {}
   common   = var.common
   eks = merge(
