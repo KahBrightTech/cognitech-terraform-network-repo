bcd2d26d1bdbf9c2b9854d9d8e3a80aa75e197c7
Brigthain Kargong <85526553+njibrigthain100@users.noreply.github.com>
2026-01-01 13:15:48 -0500

    crating rds instance

--- Terraform/formations/Shared-account/main.tf
+++ Terraform/formations/Shared-account/main.tf
@@ -683,7 +683,7 @@ module "eks" {
 #--------------------------------------------------------------------
 module "rds" {
   source   = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/RDS?ref=v1.5.73"
-  for_each = (var.rds_instances != null) ? { for item in var.rds_instances : item.create_rds_instance ? item.key : null => item if item.create_rds_instance } : {}
+  for_each = (var.rds_instances != null) ? { for item in var.rds_instances : item.key => item } : {}
   common   = var.common
   rds_instance = merge(
     each.value,
